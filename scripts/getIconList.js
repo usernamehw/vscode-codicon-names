a2cd9d8afb51f18bf8dd04c788226c40b2dc1268
al <usernamehw@gmail.com>
2021-05-03 16:09:43 +0300

    Remove csv parser

--- scripts/getIconList.js
+++ scripts/getIconList.js
@@ -1,24 +1,29 @@
+// @ts-check
 const https = require('https');
 const fs = require('fs');
-const csv = require('csv-parser');
 
-const results = [];
-const file = fs.createWriteStream("./scripts/icons.csv");
-const request = https.get("https://raw.githubusercontent.com/microsoft/vscode-codicons/master/dist/codicon.csv", (response) => {
+const csvPath = './scripts/icons.csv';
+const file = fs.createWriteStream(csvPath);
+const request = https.get('https://raw.githubusercontent.com/microsoft/vscode-codicons/master/dist/codicon.csv', (response) => {
 	response.pipe(file);
 	file.on('finish', () => {
-		console.log('✅');
-		fs.createReadStream('./scripts/icons.csv')
-			.pipe(csv())
-			.on('data', (data) => results.push(data))
-			.on('end', () => {
-				const iconNames = results.map(item => item.short_name);
-				fs.writeFile('./scripts/icons.json', JSON.stringify(iconNames), (err) => {
-					if (err) throw err;
-				});
-			});
+		console.log('✅  "icons.csv" downloaded');
+		fs.readFile(csvPath, (err, data) => {
+			if (err) {
+				throw err;
+			}
+			const lines = data
+				.toString()
+				.split('\n')
+				.map(line => line.split(',')[0]);
+			fs.writeFile('./src/iconNames.ts', `export const iconsNames = ${JSON.stringify(lines)};`, (err) => {
+				if (err) {
+					throw err;
+				}
+				console.log('✅  "iconNames.ts" written')
+			})
+		})
 	})
 }).on('error', (err) => {
-	fs.unlink(dest);
 	throw err;
 });
